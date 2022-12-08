2208deccde1dd63f8aaa1591fa7cb1d7cf7d7b14
LifeOfKnull <hunterneal1997@gmail.com>
2022-12-08 17:10:15 -0500

    Issue #2 - Added layout folder - Added RouterView to index - Tested Index styling

---
+++ client/src/layouts/default.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'DefaultLayout',
+})
+</script>
+
+<script lang="ts" setup></script>
+
+<template>
+	<RouterView />
+</template>
+
+<style lang="scss" scoped></style>
--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,1 +1,24 @@
-<template></template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'Home',
+})
+</script>
+
+<script lang="ts" setup></script>
+
+<template>
+	<button class="test">test</button>
+</template>
+
+<style lang="scss" scoped>
+.test {
+	text-transform: capitalize;
+	background-color: orange;
+	&:hover {
+		background-color: orangered;
+		color: wheat;
+	}
+}
+</style>
