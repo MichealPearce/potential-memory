63302d7f093b7e95945432c7891e960139aee8cc
MichealPearce <michealpearce@outlook.com>
2022-12-08 19:25:28 +0000

    update server project config, add path alias, and updated build path

--- server/src/types/vite.d.ts
+++ server/src/types/vite.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+
+declare const __APP_PATH__: string
+declare const __SERVER_PATH__: string
--- server/vite.config.ts
+++ server/vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import { VitePluginNode } from 'vite-plugin-node'
+import { resolve } from 'path'
 
 export default defineConfig({
+	define: {
+		__APP_PATH__: `"${resolve('../')}"`,
+		__SERVER_PATH__: `"${resolve('./')}"`,
+	},
+
+	resolve: {
+		alias: {
+			server: resolve('./src'),
+		},
+	},
+
 	plugins: VitePluginNode({
 		adapter: 'express',
-		appPath: './src/main.ts',
+		appPath: './src/index.ts',
 		tsCompiler: 'swc',
 	}),
+
+	build: {
+		outDir: '../dist',
+	},
 })
